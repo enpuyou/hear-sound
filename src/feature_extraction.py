ff0580a9a5cdc035736c6aa2cacc4b3e4b93e4e8
enpuyou <youe2@allegheny.edu>
2020-12-28 01:31:15 -0500

    remove notebook comments

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,4 +1,3 @@
-# %%
 from surfboard.sound import Waveform
 # import numpy as np
 import pandas as pd
@@ -13,15 +12,9 @@ sound = Waveform(path=path, sample_rate=44100)
 # sound = Waveform(signal=np.sin(np.arange(0, 2 * np.pi, 1/24000)), sample_rate=44100)
 
 f0_contour = sound.f0_contour()
-# %%
 # print(f0_contour)
 df = pd.DataFrame(f0_contour[0], columns=["pitch"])
 df["temp"] = df.index
 print(df)
 alt.renderers.enable('mimetype')
 alt.Chart(df).mark_line().encode(x="temp", y="pitch")
-
-
-
-
-# %%
