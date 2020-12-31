10e57620e4e7fdc4738443a96bca11a2b391fee9
enpuyou <youe2@allegheny.edu>
2020-12-30 23:16:18 -0500

    add a midi to note example

--- src/midi.py
+++ src/midi.py
@@ -56,3 +56,41 @@ while True:
     major_chord(F, 1)
     major_chord(G, 1)
     major_chord(C, 2)
+
+
+# midi to note example from https://github.com/justinsalamon/
+# audio_to_midi_melodia/blob/master/audio_to_midi_melodia.py
+def midi_to_notes(midi, fs, hop, smooth, minduration):
+
+    # smooth midi pitch sequence first
+    if (smooth > 0):
+        filter_duration = smooth  # in seconds
+        filter_size = int(filter_duration * fs / float(hop))
+        if filter_size % 2 == 0:
+            filter_size += 1
+        midi_filt = medfilt(midi, filter_size)
+    else:
+        midi_filt = midi
+    # print(len(midi),len(midi_filt))
+
+    notes = []
+    p_prev = None
+    duration = 0
+    onset = 0
+    for n, p in enumerate(midi_filt):
+        if p == p_prev:
+            duration += 1
+        else:
+            # treat 0 as silence
+            if p_prev > 0:
+                # add note
+                duration_sec = duration * hop / float(fs)
+                # only add notes that are long enough
+                if duration_sec >= minduration:
+                    onset_sec = onset * hop / float(fs)
+                    notes.append((onset_sec, duration_sec, p_prev))
+
+            # start new note
+            onset = n
+            duration = 1
+            p_prev = p
