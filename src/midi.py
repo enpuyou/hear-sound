1819624362a89833183214770473e87a06c2338b
enpuyou <youe2@allegheny.edu>
2021-02-23 02:01:47 -0500

    Add function to convert midi number to note

--- src/midi.py
+++ src/midi.py
@@ -12,6 +12,14 @@ G = 55
 A = 57
 F = 53
 
+NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+
+def midi_number_to_note(number):
+    octave = number // len(NOTES) - 1
+    note = NOTES[number % len(NOTES)]
+    return note + str(octave)
+
 
 # some example using mido from
 # https://natespilman.com/blog/playing-chords-with-mido-and-python/
@@ -22,7 +30,8 @@ def note(note, velocity=64, time=2):
     channel: 0..15
     time: "not included in the encoded message"
     """
-    return mido.Message("note_on", note=note, velocity=velocity, time=time)
+    message = mido.Message("note_on", note=note, velocity=velocity, time=time)
+    return message
 
 
 def note_off(note, velocity=64, time=2):
@@ -213,19 +222,19 @@ def fifth_scale_down(root, velocity=64, duration=0.1):
         sleep(random.randint(1, 20)/100)
 
 
-def triad_octave(root, velocity=64, duration=0.1):
-    for i in range(10):
-        if root - i * 7 - 5 < 0:
-            break
-        # dom_seventh(root + i * 12, velocity)
-        outport.send(note(root - i * 7, velocity))
-        sleep(random.randint(1, 10)/100)
-        outport.send(note(root - i * 7, velocity))
-        sleep(random.randint(1, 10)/100)
-        outport.send(note(root - i * 7 - 5, velocity))
-        sleep(random.randint(1, 10)/100)
-        outport.send(note(root - i * 7 - 5, velocity))
-        sleep(0.2)
+# def triad_octave(root, velocity=64, duration=0.1):
+#     for i in range(10):
+#         if root - i * 7 - 5 < 0:
+#             break
+#         # dom_seventh(root + i * 12, velocity)
+#         outport.send(note(root - i * 7, velocity))
+#         sleep(random.randint(1, 10)/100)
+#         outport.send(note(root - i * 7, velocity))
+#         sleep(random.randint(1, 10)/100)
+#         outport.send(note(root - i * 7 - 5, velocity))
+#         sleep(random.randint(1, 10)/100)
+#         outport.send(note(root - i * 7 - 5, velocity))
+#         sleep(0.2)
         # sleep(random.randint(1, 10)/100)
         # outport.send(note_off(root + i * 7, velocity))
 
@@ -243,21 +252,25 @@ def triad_octave(root, velocity=64, duration=0.1):
 
 def play(root, velocity=64, duration=0.2):
     # if root % 2 == 1:
-    #     fourth_scale_down(root, velocity)
-    #     minor_chord(root, velocity, duration)
-        # fourth_scale(root, velocity)
+    #     # fourth_scale_down(root, velocity)
+    #     # minor_chord(root, velocity, duration)
+    #     fourth_scale(root, velocity)
+    #     # fourths(root, velocity)
     # else:
-        # major_seventh(root, velocity, duration)
-        # sleep(0.1)
-        # minor_seventh(root + 14, velocity, duration)
-        # sleep(0.15)
-        # major_seventh(root + 7, velocity, duration)
-        # sleep(0.1)
-        # fifths(root, velocity, duration)
-        # fifth_scale(root, velocity)
-        # fifth_scale(root, velocity, duration)
+    #     major_seventh(root, velocity, duration)
+    #     sleep(0.1)
+    #     minor_seventh(root + 14, velocity, duration)
+    #     sleep(0.15)
+    #     major_seventh(root + 7, velocity, duration)
+    #     sleep(0.1)
+    #     fifths(root, velocity, duration)
+    #     fifth_scale(root, velocity)
+    #     fifth_scale(root, velocity, duration)
+    outport.send(note(root, velocity))
+    sleep(10)
+    outport.send(note_off(root, velocity))
     # fourth_scale(root, velocity, duration)
-    triad_octave(root, velocity)
+    # triad_octave(root, velocity)
 
 
 # midi to note example from https://github.com/justinsalamon/
